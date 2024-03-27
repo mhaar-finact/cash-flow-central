1fe681e03d2b52e0e4c4078f031e93dd0ced84c9
matt.haar@fiserv.com <matt.haar@fiserv.com>
2024-03-27 13:28:43 -0400

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0MTE4NA/spectral.js?branch_slug=develop&token=465d34ea-79cf-4e9d-9f22-b88d83c93548";
+export default { extends: ruleset };
